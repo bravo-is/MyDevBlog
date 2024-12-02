7c8587a8cc4eab66826af08845f0f8059ff29947
Israel <bravoil@mail.uc.edu>
2024-12-01 20:53:16 -0500

    refactor: Simplify tag display in Tags component and MarkdownPostLayout; remove dynamic class logic and enhance styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],  
-	safelist: [
-		'bg-neutral-200',
-		'bg-neutral-300',
-		'bg-neutral-400',
-		'bg-neutral-500',
-		'bg-neutral-600',
-		'bg-neutral-700',
-		'bg-neutral-800',
-		'bg-neutral-900',
-	  ],
 	darkMode: "class",
 	theme: {
 		extend: {},
